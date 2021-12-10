bb3338c7c139d97c2fd29869c1d700de76fd5983
Renan Queiroz <renanlq@gmail.com>
2021-12-09 21:27:30 -0300

    Update bash

--- bash/salesforce-build/build.sh
+++ bash/salesforce-build/build.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 branch="master"
+buildpath="build"
 
 echo $(date '+%Y-%m-%d %H:%M:%S:%s')": clean folder: build";
-rm -rf build/;
-mkdir build;
+rm -rf $buildpath;
+mkdir $buildpath;
 
-echo $(date '+%Y-%m-%d %H:%M:%S:%s')": update branch $branch";
-git reset --hard;
-git checkout $branch;
-git pull --force;
+# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": update branch $branch";
+# git reset --hard;
+# git checkout $branch;
+# git pull --force;
 
-echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get commit list from baseline, see baseline.txt and commitlist.txt";
-hashbaseline=$(head -n 1 baseline.txt);
-rm commitlist.txt;
-git rev-list $hashbaseline..HEAD --oneline > commitlist.txt;
+# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get commit list from baseline, see baseline.txt and commitlist.txt";
+# hashbaseline=$(head -n 1 baseline.txt);
+# rm commitlist.txt;
+# #git rev-list HEAD -n 1 --oneline > commitlist.txt;
+# git rev-list $hashbaseline..HEAD --oneline > commitlist.txt;
 
-echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get files from the list of commits, see in commitedfiles.txt";
-while read commit message
-do
-    git diff-tree --name-status --no-commit-id -r $commit >> commitedfiles.txt;
-done < commitlist.txt;
+# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get files from the list of commits, see in commitedfiles.txt";
+# while read commit message
+# do
+#     git diff-tree --name-status --no-commit-id -r $commit >> commitedfiles.txt;
+# done < commitlist.txt;
 
-echo $(date '+%Y-%m-%d %H:%M:%S:%s')": create dummy sfdx project";
-sfdx force:project:create -n build --template standard
+# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": create dummy sfdx project";
+# sfdx force:project:create -n build --template standard
 
 echo $(date '+%Y-%m-%d %H:%M:%S:%s')": copy files to build folder";
-while read status path
+while read status filepath
 do
-    if [ $status != "D" ]; then
-        mkdir -p "$(dirname "build/$path")";
-        cp -r "../$path" "build/$path";
+
+    if [ $status != "D" ]; then   
+        if [[ "$filepath" == *\/* ]]; then
+            mkdir -p "$(dirname "$buildpath/${filepath%\/*}")"
+        fi
+        cp -r "../../$filepath" "$buildpath/$filepath"
+    else
+        #//TODO Add package-destructive.xml
+        echo "destructive: $filepath"
     fi
 done < commitedfiles.txt;
