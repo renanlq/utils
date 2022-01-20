60a4517aa5794467cd7a4b952574b98ffd5979f9
Renan Queiroz <renanlq@gmail.com>
2022-01-20 17:28:54 -0300

    Update build.sh

--- bash/salesforce-build/build.sh
+++ bash/salesforce-build/build.sh
@@ -6,25 +6,25 @@ echo $(date '+%Y-%m-%d %H:%M:%S:%s')": clean folder: build";
 rm -rf $buildpath;
 mkdir $buildpath;
 
-# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": update branch $branch";
-# git reset --hard;
-# git checkout $branch;
-# git pull --force;
+echo $(date '+%Y-%m-%d %H:%M:%S:%s')": update branch $branch";
+git reset --hard;
+git checkout $branch;
+git pull --force;
 
-# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get commit list from baseline, see baseline.txt and commitlist.txt";
-# hashbaseline=$(head -n 1 baseline.txt);
-# rm commitlist.txt;
-# #git rev-list HEAD -n 1 --oneline > commitlist.txt;
-# git rev-list $hashbaseline..HEAD --oneline > commitlist.txt;
+echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get commit list from baseline, see baseline.txt and commitlist.txt";
+hashbaseline=$(head -n 1 baseline.txt);
+rm commitlist.txt;
+#git rev-list HEAD -n 1 --oneline > commitlist.txt;
+git rev-list $hashbaseline..HEAD --oneline > commitlist.txt;
 
-# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get files from the list of commits, see in commitedfiles.txt";
-# while read commit message
-# do
-#     git diff-tree --name-status --no-commit-id -r $commit >> commitedfiles.txt;
-# done < commitlist.txt;
+echo $(date '+%Y-%m-%d %H:%M:%S:%s')": get files from the list of commits, see in commitedfiles.txt";
+while read commit message
+do
+    git diff-tree --name-status --no-commit-id -r $commit >> commitedfiles.txt;
+done < commitlist.txt;
 
-# echo $(date '+%Y-%m-%d %H:%M:%S:%s')": create dummy sfdx project";
-# sfdx force:project:create -n build --template standard
+echo $(date '+%Y-%m-%d %H:%M:%S:%s')": create dummy sfdx project";
+sfdx force:project:create -n build --template standard
 
 echo $(date '+%Y-%m-%d %H:%M:%S:%s')": copy files to build folder";
 while read status filepath
