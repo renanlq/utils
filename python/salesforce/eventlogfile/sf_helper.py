57bb3ab8f02ebdfd9cf28c76caaec81f0638d106
Renan Queiroz <renanlq@gmail.com>
2024-03-14 23:19:16 -0300

    private attributes to sf_helper

--- python/salesforce/eventlogfile/sf_helper.py
+++ python/salesforce/eventlogfile/sf_helper.py
@@ -7,20 +7,20 @@ class SalesforceHelper:
         raise NotImplementedError()
 
     def __init__(self, domain, sf_consumer_key, sf_consumer_secret, api_version):
-        self.domain = domain
-        self.sf_consumer_key = sf_consumer_key
-        self.sf_consumer_secret = sf_consumer_secret
-        self.api_version = api_version
-        self.auth_salesforce()
+        self.__domain = domain
+        self.__sf_consumer_key = sf_consumer_key
+        self.__sf_consumer_secret = sf_consumer_secret
+        self.__api_version = api_version
+        self.__auth_salesforce()
 
-    def auth_salesforce(self):
+    def __auth_salesforce(self):
         try:
-            self.sf_auth_url = self.domain + '/services/oauth2/token'
+            self.__sf_auth_url = self.__domain + '/services/oauth2/token'
             http_response = requests.post(
-                self.sf_auth_url, data = {
+                self.__sf_auth_url, data = {
                     'grant_type': 'client_credentials',
-                    'client_id': self.sf_consumer_key,
-                    'client_secret': self.sf_consumer_secret
+                    'client_id': self.__sf_consumer_key,
+                    'client_secret': self.__sf_consumer_secret
                     })
 
             if "access_token" not in http_response.json() and not http_response.json()["access_token"]:
@@ -29,9 +29,9 @@ class SalesforceHelper:
 
             json_response = http_response.json()
             sf_access_token = json_response['access_token']
-            self.sf_instance_url = json_response['instance_url']
+            self.__sf_instance_url = json_response['instance_url']
 
-            self.sf_auth_headers = {
+            self.__sf_auth_headers = {
                 'Authorization':'Bearer ' + sf_access_token, 
                 'Content-type': 'application/json',
                 'Accept-Encoding': 'gzip'
@@ -42,9 +42,9 @@ class SalesforceHelper:
         
     def query(self, query):
         try:
-            query_url =  self.sf_instance_url + '/services/data/v'+self.api_version+'/query/'
+            query_url =  self.__sf_instance_url + '/services/data/v'+self.__api_version+'/query/'
             parameters = {'q': query }     
-            http_response = requests.request('get', query_url, headers = self.sf_auth_headers, params = parameters, timeout = 30) 
+            http_response = requests.request('get', query_url, headers = self.__sf_auth_headers, params = parameters, timeout = 30) 
             return http_response.json()['records']
         except BaseException as error:
             print('query error')
@@ -52,8 +52,8 @@ class SalesforceHelper:
     
     def get_file(self, file):
         try:
-            file_url = self.sf_instance_url + file 
-            return requests.request('get', file_url, headers = self.sf_auth_headers, timeout = 30)
+            file_url = self.__sf_instance_url + file 
+            return requests.request('get', file_url, headers = self.__sf_auth_headers, timeout = 30)
         except BaseException as error:
             print('get file error')
             raise error
